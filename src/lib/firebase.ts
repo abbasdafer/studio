7d754da14e06317c783a1805a260801bc3432ace
AMC for games <hussein.mansour132.2000.3@gmail.com>
2025-08-11 18:11:49 +0000

    المستخدمين يشتكون من انه في كل مرة يريدون استخدام البرنامج يضطرون لتسجيل

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -15,7 +15,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+
+// Initialize Firebase Auth with persistence
+const auth = initializeAuth(app, {
+  persistence: browserLocalPersistence,
+});
+
 const db = getFirestore(app);
 
 export { app, auth, db };
